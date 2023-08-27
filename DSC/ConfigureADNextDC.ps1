42c66871ecf00c2cbcbe473e4bcc0b29c48fbc0a
Steve Bostedor <steve@bostedor.com>
2023-08-27 15:59:53 -0400

    More bugs

--- DSC/ConfigureADNextDC.ps1
+++ DSC/ConfigureADNextDC.ps1
@@ -9,9 +9,6 @@
         [String]$DNSServer,
 
         [Parameter(Mandatory)]
-        [String]$DNSForwarder,
-
-        [Parameter(Mandatory)]
         [System.Management.Automation.PSCredential]$Admincreds,
 
         [Int]$RetryCount=20,
@@ -45,86 +42,6 @@
             DependsOn = "[WindowsFeature]DNS"
         }
         
-        Script SetDNSForwarder
-        {
-            #
-            # 
-            #
-            SetScript =
-            {
-                $dnsrunning = $false
-                $triesleft = $Using:RetryCount
-                Write-Verbose -Verbose "Checking if DNS service is running."
-                While (-not $dnsrunning -and ($triesleft -gt 0))
-                {
-                    $triesleft--
-                    try
-                    {
-                        $dnsrunning = (Get-Service -name dns).Status -eq "running"
-                    } catch {
-                        $dnsrunning = $false
-                    }
-                    if (-not $dnsrunning)
-                    {
-                        Write-Verbose -Verbose "Waiting $($Using:RetryIntervalSec) seconds for DNS service to start"
-                        Start-Sleep -Seconds $Using:RetryIntervalSec
-                    }
-                }
-
-                $triesleft = $Using:RetryCount
-                Write-Verbose "Checking if DNS is responding to WMI."
-                do {
-                    #
-                    # Get-DNSForwarderList would sometimes hang directly after boot.
-                    # So, try something with a reasonable timeout first.
-                    #
-                    try {
-                        Write-Verbose -Verbose "Reading DNS through WMI to see if it responds."
-                        $dnsObject = Get-CimInstance -ClassName microsoftdns_server -Namespace root/microsoftdns -OperationTimeoutSec 10
-                    } catch {
-                        $dnsobject = $false
-                        Write-Warning -Verbose "Get-Ciminstance for DNS failed: $_"
-                    }
-                    if ($dnsObject)
-                    {
-                        $dnsrunning = $true
-                    } else {
-                        $dnsrunning = $false
-                        Write-Verbose -Verbose "Waiting $($Using:RetryIntervalSec) seconds for WMI starting to respond"
-                        Start-Sleep -Seconds $Using:RetryIntervalSec
-                    }
-                    $triesleft--
-                } while (-not $dnsrunning -and ($triesleft -gt 0))
-
-                if (-not $dnsrunning)
-                {
-                    Write-Warning "DNS service is not running, cannot edit forwarder. Template deployment will fail."
-                    # but continue anyway.
-                }
-                try {
-                    Write-Verbose -Verbose "Getting list of DNS forwarders"
-                    $forwarderlist = Get-DnsServerForwarder
-                    if ($forwarderlist.IPAddress)
-                    { 
-                        Write-Verbose -Verbose "Removing forwarders"
-                        Remove-DnsServerForwarder -IPAddress $forwarderlist.IPAddress -Force
-                    } else {
-                        Write-Verbose -Verbose "No forwarders found"
-                    }
-                } catch {
-                    Write-Warning -Verbose "Exception running Remove-DNSServerForwarder: $_"
-                }
-                try {
-                    Write-Verbose -Verbose "setting  forwarder to $($using:DNSForwarder)"
-                    Set-DnsServerForwarder -IPAddress $using:DNSForwarder
-                } catch {
-                    Write-Warning -Verbose "Exception running Set-DNSServerForwarder: $_"
-                }                 
-            }
-            GetScript =  { @{} }
-            TestScript = { $false }
-            DependsOn = "[WindowsFeature]DNSTools"
-        }
         
         WindowsFeature ADTools
         {
@@ -194,7 +111,7 @@
             DomainUserCredential= $DomainCreds
             RetryCount = $RetryCount
             RetryIntervalSec = $RetryIntervalSec
-            DependsOn = @("[WindowsFeature]ADDSInstall", "[xDnsServerAddress]DnsServerAddress", "[Script]SetDNSForwarder", "[PendingReboot]AfterMountingDisk")
+            DependsOn = @("[WindowsFeature]ADDSInstall", "[xDnsServerAddress]DnsServerAddress", "[PendingReboot]AfterMountingDisk")
         }
         
         xADDomainController NextDC
@@ -205,7 +122,7 @@
             DatabasePath = "F:\NTDS"
             LogPath = "F:\NTDS"
             SysvolPath = "F:\SYSVOL"
-            DependsOn = @("[xWaitForADDomain]DscForestWait", "[WindowsFeature]ADDSInstall", "[Script]SetDNSForwarder")
+            DependsOn = @("[xWaitForADDomain]DscForestWait", "[WindowsFeature]ADDSInstall")
         }
                 
         #
